c8159002aff1fc567ebbeabff1fcb17220190a74
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-17 11:12:59 +0100

    Repository created for Customer. Customers can now be saved to the database.

--- RestaurantBooking/src/main/java/com/codeclan/finalProject/RestaurantBooking/models/Customer.java
+++ RestaurantBooking/src/main/java/com/codeclan/finalProject/RestaurantBooking/models/Customer.java
@@ -1,9 +1,23 @@
 package com.codeclan.finalProject.RestaurantBooking.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "contact")
     private String contact;
+
+    @Column(name = "no_of_visits")
     private int noOfVisits;
 
     public Customer(String name, String contact) {
@@ -13,6 +27,14 @@ public class Customer {
     }
 
     public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
--- RestaurantBooking/src/test/java/com/codeclan/finalProject/RestaurantBooking/RestaurantBookingApplicationTests.java
+++ RestaurantBooking/src/test/java/com/codeclan/finalProject/RestaurantBooking/RestaurantBookingApplicationTests.java
@@ -1,16 +1,22 @@
 package com.codeclan.finalProject.RestaurantBooking;
 
 import com.codeclan.finalProject.RestaurantBooking.models.Customer;
+import com.codeclan.finalProject.RestaurantBooking.repositories.CustomerRepository.CustomerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestaurantBookingApplicationTests {
+
+	@Autowired
+	CustomerRepository customerRepository;
 
 	@Test
 	public void contextLoads() {
@@ -31,7 +37,15 @@ public class RestaurantBookingApplicationTests {
 	@Test
 	public void customerHasNoVisitsInitially() {
 		Customer customer = new Customer("Bob", "07999999999");
-		assertEquals(0, customer.getNoOfVisits());		
+		assertEquals(0, customer.getNoOfVisits());
+	}
+
+	@Test
+	public void customerCanBeSaved() {
+		Customer customer = new Customer("Bob", "07999999999");
+		assertEquals(null, customer.getId());
+		customerRepository.save(customer);
+		assertNotNull(customer.getId());
 	}
 
 }
